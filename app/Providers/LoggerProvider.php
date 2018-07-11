ceb502bedd51278ab222f18715ce445a8a8e1ad0
xiaolin <462441355@qq.com>
2018-07-12 07:29:36 +0800

    * 增加sql记录

--- app/Providers/LoggerProvider.php
+++ app/Providers/LoggerProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use App\Support\Logger\Factory;
 use App\Support\Logger\Local;
 use Illuminate\Support\ServiceProvider;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
 
 class LoggerProvider extends ServiceProvider
 {
@@ -34,13 +37,28 @@ class LoggerProvider extends ServiceProvider
      */
     public function registerLoggerDebug()
     {
-        
+
         $this->app->bind('logger_local', function () {
             return new Local();
         });
 
         $this->app->bind('logger_factory', function () {
             return new Factory();
+        });
+
+        $this->app->bind('logger_sql', function () {
+            $monolog = new Logger('debug');
+            $log_level = Logger::DEBUG;
+
+            $log_path = storage_path('logs') . '/' . str_replace('logger_', '', 'sql') . '.log';
+
+            $handler = new RotatingFileHandler($log_path, 5, $log_level, true, 0777);
+            //$handler->setFilenameFormat('{filename}-{date}-'.date('H'), 'Y-m-d');
+            $monolog->pushHandler($handler);
+            $formatter = new LineFormatter(null, null, true, true);
+            $handler->setFormatter($formatter);
+
+            return $monolog;
         });
     }
 
