c499322a6ccbe7f70bee83262fc5de4fd9d43742
xiaolin <462441355@qq.com>
2018-07-24 16:17:51 +0800

    * 测试api移出

--- routes/kong.php
+++ routes/kong.php
@@ -8,18 +8,16 @@
 // 用户登录
 Route::post('/user/login', 'UserController@login')->name('user.login');
 
+// Kong测试api
+Route::any('/index/kong', 'IndexController@kong')->name('index.kong');
+Route::any('/index/add', 'IndexController@add')->name('index.add');
+
 Route::group(['middleware' => 'auth.kong'], function () {
     // 用户模块
     Route::any('/user/status', 'UserController@status')->name('user.add');
     Route::any('/user/lists', 'UserController@lists')->name('user.lists');
     Route::post('/user/add', 'UserController@add')->name('user.add');
     Route::get('/user/info', 'UserController@info')->name('user.info');
-
-
-
-    // Kong测试api
-    Route::any('/index/kong', 'IndexController@kong')->name('index.kong');
-    Route::any('/index/add', 'IndexController@add')->name('index.add');
 
     // Kong Service 模块
     Route::any('/service/lists', 'ServiceController@lists')->name('service.lists');
