d9127071498f42af1895c2ca7e918b061fa03fc0
xiaolin <462441355@qq.com>
2018-08-01 14:43:36 +0800

    * add

--- database/migrations/2018_07_26_101459_create_router_table.php
+++ database/migrations/2018_07_26_101459_create_router_table.php
@@ -17,14 +17,15 @@ class CreateRouterTable extends Migration
             $table->increments('id');
             $table->bigInteger('pid');
             $table->bigInteger('level');
-            $table->string('name',32);
-            $table->string('code',64);
-            $table->string('route',300);
-            $table->string('res_uri',300);
-            $table->string('icon',32);
-            $table->string('is_hidden',32)->default(false);
-            $table->string('route',64);
-            $table->tinyInteger('type',4);
+            $table->string('name', 32);
+            $table->string('code', 64);
+            $table->string('route', 300);
+            $table->string('res_uri', 300);
+            $table->string('icon', 32);
+            $table->string('is_hidden', 32)->default(false);
+            $table->string('noDropdown', 32)->default(false);
+            $table->string('route', 64);
+            $table->tinyInteger('type', 4);
             $table->timestamps();
         });
     }
